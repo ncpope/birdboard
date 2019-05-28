7c2270956651844381c7c2ce8b65d923573198cd
Nathan Pope <ncpope@gmail.com>
2019-05-28 16:51:24 +0200

    initial commit

---
+++ tests/Feature/ProjectsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ProjectsTest extends TestCase
+{
+    /** @test */
+    public function a_user_can_create_a_project()
+    {
+        
+    }
+}
