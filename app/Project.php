ad2e2f2839660945581b9f3926f81fbcd180a2bd
Nathan Pope <ncpope@gmail.com>
2019-05-29 18:02:57 +0200

    Tested creation of a project and viewing a single project. Created a path() shorthand method for generating a path to a project.

--- app/Project.php
+++ app/Project.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+    public $table = 'projects';
+
     protected $guarded = [];
+
+    public function path()
+    {
+        return "/projects/{$this->id}";
+    }
 }
