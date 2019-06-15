80afe91ce7c42a542cd323db8c4c785b8be4d88e
Nathan Pope <ncpope@gmail.com>
2019-06-15 11:17:34 +0200

    trying trave ci

--- config/database.php
+++ config/database.php
@@ -43,6 +43,18 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_TEST_HOST', 'localhost'),
+            'database'  => env('DB_TEST_DATABASE', 'homestead_test'),
+            'username'  => env('DB_TEST_USERNAME', 'homestead'),
+            'password'  => env('DB_TEST_PASSWORD', 'secret'),
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+            'strict'    => false,
+        ],
+
 		'sqlite_testing' => [
 			'driver'   => 'sqlite',
 			'database' => storage_path().'/testing.sqlite',
